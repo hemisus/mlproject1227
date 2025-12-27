9904db9b6558eebdc1ee2b947654ddf2a18a1790
Hemisus <hydro6323@gmail.com>
2025-12-27 14:31:39 +0900

    EDA and Problem statement

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()   #3가지 정보가 있을텐데, 그 중 마지막 정보가 필요
